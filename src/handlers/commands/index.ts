cb89021c3ab0fe0480cb03b5bb42ae25bcc99399
Bekzod <bekzodturgunoff@gmail.com>
2026-06-04 11:59:06 +0500

    workers

--- src/handlers/commands/index.ts
+++ src/handlers/commands/index.ts
@@ -1,4 +1,5 @@
 import type {Bot} from "grammy/web";
+import {setupRouter} from "./router.js";
 import {setupPublicCommands} from "./public-commands.js";
 import {setupOwnerInfoCommands} from "./owner-info.js";
 import {setupOwnerManageCommands} from "./owner-manage.js";
@@ -6,6 +7,7 @@ import {setupOwnerActionCommands} from "./owner-action.js";
 import {setupDmHandler} from "./dm-handler.js";
 
 export function setupTelegramHandlers(bot: Bot): void {
+  setupRouter(bot);
   setupPublicCommands(bot);
   setupOwnerInfoCommands(bot);
   setupOwnerManageCommands(bot);
